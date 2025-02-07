472e2ec4c8eab3e5fc3cf074a06c35ce7a63bc74
Desire Gwanzura <112183262+GuanKid@users.noreply.github.com>
2025-02-07 09:12:43 +0200

    Add files via upload

---
+++ src/main/java/com/taskmanager/task_manager/controller/TaskController.java
@@ -0,0 +1,41 @@
+package com.taskmanager.task_manager.controller;
+
+import com.taskmanager.task_manager.model.Task;
+import com.taskmanager.task_manager.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+@CrossOrigin(origins = "http://localhost:3000") // Allow frontend access
+public class TaskController {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable String id, @RequestBody Task taskDetails) {
+        Task task = taskRepository.findById(id).orElseThrow();
+        task.setTitle(taskDetails.getTitle());
+        task.setDescription(taskDetails.getDescription());
+        task.setCompleted(taskDetails.isCompleted());
+        return taskRepository.save(task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable String id) {
+        taskRepository.deleteById(id);
+    }
+}
